931d068576bdf1becba87968695490117964ed97
Om Singh Chouhan <omchouhan227@gmail.com>
2026-06-21 14:44:52 +0530

    refactor: replace radix-ui umbrella package with scoped imports

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import * as React from "react"
-import { Tooltip as TooltipPrimitive } from "radix-ui"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/lib/utils"
+import { cn }  from "@/lib/utils"
 
 function TooltipProvider({
   delayDuration = 0,
